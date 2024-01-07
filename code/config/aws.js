8f1b42e975adc5ed46f88324b1a16fc54c04f502
Shenal Mendis <115540212+Shenal-M@users.noreply.github.com>
2024-01-07 14:09:57 +0530

    Update aws.js

--- code/config/aws.js
+++ code/config/aws.js
@@ -3,8 +3,8 @@ const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 
 // Hardcoded AWS credentials (for local development only)
 const credentials = ({
-  accessKeyId: 'AKIATLP2N6KZMF2HXW7Y',
-  secretAccessKey: '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',
+  accessKeyId: test,
+  secretAccessKey: test,
 });
 
 const dynamodb = new DynamoDB({
